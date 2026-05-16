cbf8fc5f206b03024c7f4ba5517bcd766c02c502
Espy <ibrahimsamf23@gmail.com>
2026-05-17 01:32:14 +0300

    responsivite pour mobile

---
+++ copy_to_professional.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Copier le fichier final vers index_professional.html
+copy "c:\Users\САМАКЕ ИБРАНИМ\Desktop\projet_fin_anne\index_final.html" "c:\Users\САМАКЕ ИБРАНИМ\Desktop\projet_fin_anne\index_professional.html"
+echo ✓ index_professional.html updated
+
+REM Lister les fichiers
+echo.
+echo Fichiers index actuels:
+dir "c:\Users\САМАКЕ ИБРАНИМ\Desktop\projet_fin_anne\index*.html"
+pause
